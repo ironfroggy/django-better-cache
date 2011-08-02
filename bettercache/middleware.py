d188f1519629b50e67f420b34c8e1c0dcf93aede
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-08-02 10:37:54 -0400

    [CMSPERF-21] removed uneeded __init__

--- bettercache/middleware.py
+++ bettercache/middleware.py
@@ -5,38 +5,34 @@ from django.utils.cache import patch_response_headers, get_max_age, learn_cache_
 from bettercache.utils import set_post_pre_check_headers
 
 class BetterUpdateCacheMiddleware(UpdateCacheMiddleware):
-    def __init__(self):
-        super(BetterUpdateCacheMiddleware, self).__init__()
-        self.post_check = settings.CACHE_POSTCHECK_SECONDS
 
-
-        def process_response(self, request, response):
-            """ Sets the cache, if needed.
-                Copied from django so headers are only updated when appropriate.
-            """                                                                             
-            if not self._should_update_cache(request, response):
-                # We don't need to update the cache, just return.                                                        
-                return response                                                                                          
-            if not response.status_code == 200:
-                return response                                                                                          
-            # Try to get the timeout from the "max-age" section of the "Cache-
-            # Control" header before reverting to using the default cache_timeout                                        
-            # length.                                                                                                    
-            timeout = get_max_age(response)                                                                              
-            if timeout == None:
-                timeout = self.cache_timeout                                                                             
-            elif timeout == 0:
-                # max-age was set to 0, don't bother caching.
-                return response
-            patch_response_headers(response, timeout)
-            # This is the only difference from django for now
-            set_post_pre_check_headers(response)
-            if timeout:
-                cache_key = learn_cache_key(request, response, timeout, self.key_prefix, cache=self.cache)               
-                if hasattr(response, 'render') and callable(response.render):                                            
-                    response.add_post_render_callback(
-                        lambda r: self.cache.set(cache_key, r, timeout)                                                  
-                    )
-                else:                                                                                                    
-                    self.cache.set(cache_key, response, timeout)
-            return response 
+    def process_response(self, request, response):
+        """ Sets the cache, if needed.
+            Copied from django so headers are only updated when appropriate.
+        """                                                                             
+        if not self._should_update_cache(request, response):
+            # We don't need to update the cache, just return.                                                        
+            return response                                                                                          
+        if not response.status_code == 200:
+            return response                                                                                          
+        # Try to get the timeout from the "max-age" section of the "Cache-
+        # Control" header before reverting to using the default cache_timeout                                        
+        # length.                                                                                                    
+        timeout = get_max_age(response)                                                                              
+        if timeout == None:
+            timeout = self.cache_timeout                                                                             
+        elif timeout == 0:
+            # max-age was set to 0, don't bother caching.
+            return response
+        patch_response_headers(response, timeout)
+        # This is the only difference from django for now
+        set_post_pre_check_headers(response)
+        if timeout:
+            cache_key = learn_cache_key(request, response, timeout, self.key_prefix, cache=self.cache)               
+            if hasattr(response, 'render') and callable(response.render):                                            
+                response.add_post_render_callback(
+                    lambda r: self.cache.set(cache_key, r, timeout)                                                  
+                )
+            else:                                                                                                    
+                self.cache.set(cache_key, response, timeout)
+        return response 
