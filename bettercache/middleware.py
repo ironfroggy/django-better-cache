44b1ae5a40db6d8b777474a8a413f8eb7ee6da2a
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-08-12 10:23:55 -0400

    [CMSPERF-21]the cache key was set when the middleware was instatiated which was before per_request_settings

--- bettercache/middleware.py
+++ bettercache/middleware.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.middleware.cache import UpdateCacheMiddleware
-from django.utils.cache import patch_response_headers, get_max_age, learn_cache_key
+from django.middleware.cache import UpdateCacheMiddleware, FetchFromCacheMiddleware
+from django.utils.cache import patch_response_headers, get_cache_key, get_max_age, learn_cache_key
 
 from bettercache.utils import set_post_pre_check_headers
 
@@ -33,11 +33,40 @@ class BetterUpdateCacheMiddleware(UpdateCacheMiddleware):
         # This is the only difference from django for now
         self.set_headers(request, response)
         if timeout:
-            cache_key = learn_cache_key(request, response, timeout, self.key_prefix, cache=self.cache)               
+            cache_key = learn_cache_key(request, response, timeout, settings.CACHE_MIDDLEWARE_KEY_PREFIX, cache=self.cache)               
             if hasattr(response, 'render') and callable(response.render):                                            
                 response.add_post_render_callback(
                     lambda r: self.cache.set(cache_key, r, timeout)                                                  
                 )
             else:                                                                                                    
                 self.cache.set(cache_key, response, timeout)
-        return response 
+        return response
+
+class BetterFetchFromCacheMiddleware(FetchFromCacheMiddleware):
+    def process_request(self, request):
+        """
+        Checks whether the page is already cached and returns the cached
+        version if available.
+        """
+        if not request.method in ('GET', 'HEAD'):
+            request._cache_update_cache = False
+            return None # Don't bother checking the cache.
+
+        # try and get the cached GET response
+        cache_key = get_cache_key(request, settings.CACHE_MIDDLEWARE_KEY_PREFIX, 'GET', cache=self.cache)
+        if cache_key is None:
+            request._cache_update_cache = True
+            return None # No cache information available, need to rebuild.
+        response = self.cache.get(cache_key, None)
+        # if it wasn't found and we are looking for a HEAD, try looking just for that
+        if response is None and request.method == 'HEAD':
+            cache_key = get_cache_key(request, self.key_prefix, 'HEAD', cache=self.cache)
+            response = self.cache.get(cache_key, None)
+
+        if response is None:
+            request._cache_update_cache = True
+            return None # No cache information available, need to rebuild.
+        # hit, return cached response
+        request._cache_update_cache = False
+        return response
+
