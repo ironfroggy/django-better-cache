4ee207783a9668c2c453f31d5cab511e9c8e8af8
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-11-21 14:17:54 -0500

    [CMSPERF-93] started to write cache view

--- bettercache/views.py
+++ bettercache/views.py
@@ -1,1 +1,19 @@
-"""There are no views in this app."""
+from bettercache.utils import CachingMixin
+
+
+class BetterView(CachingMixin):
+    def get(self, request):
+        response = None
+        #should this bypass
+        if not self.should_bypass_cache(request):
+            response, expired = self.get_cache(request)
+            # send off the celery task if it's expired
+            if expired:
+                GeneratePage.apply_async((strip_wsgi(request),))
+                self.set_cache(request, response)
+
+        # if response is still none we have to proxy
+        if response is None:
+            response = self.proxy(request)
+
+        return response
