8539d50b1bf2aec72dd469548b5d411fb2a2324a
Calvin Spealman <calvin@pantechnoco.com>
2011-11-27 09:20:36 -0500

    Edit README to change future plans

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bettercache',
+    version='0.2',
+    description='A replacement template cache',
+    author='Calvin Spealman',
+    author_email='ironfroggy@gmail.com',
+    url='http://github.com/ironfroggy/django-better-cache',
+    packages=['bettercache'],
+)
