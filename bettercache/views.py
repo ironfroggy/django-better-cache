0308f8ca1c258eb237d62585e766d5663378f370
Alex Dreyer <alex.dreyer@cmgdigital.com>
2012-01-18 12:02:22 -0500

    [CMSPERF-189] now actually call it

--- bettercache/views.py
+++ bettercache/views.py
@@ -13,21 +13,21 @@ class BetterView(CachingMixin):
             response, expired = self.get_cache(request)
             # send off the celery task if it's expired
             if expired:
-                logger.error("sending task for %s" %request.build_absolute_uri)
+                logger.error("sending task for %s" %request.build_absolute_uri())
                 self.send_task(request, response)
             else:
-                logger.error("sending task for %s" %request.build_absolute_uri)
+                logger.error("sending task for %s" %request.build_absolute_uri())
 
         # if response is still none we have to proxy
         if response is None:
-            logger.info('request %s proxied' %request.build_absolute_uri)
+            logger.info('request %s proxied' %request.build_absolute_uri())
             response = proxy(request)
             #TODO: delete the following two lines
             #self.set_cache(request, response)
             response['X-Bettercache-Proxy'] = 'true'
         else:
             response['X-Bettercache-Proxy'] = 'false'
-            logger.info('request %s from cache' %request.build_absolute_uri)
+            logger.info('request %s from cache' %request.build_absolute_uri())
 
         return response
 
