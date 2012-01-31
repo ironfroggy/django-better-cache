bb8dc52f76d63d9b1bf6e5be03c992c4dffde621
Calvin Spealman <calvin@pantechnoco.com>
2012-01-31 00:41:20 -0500

    [release/0.5.1] Marking 0.5.1 release in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='bettercache',
-    version='0.5',
+    version='0.5.1',
     description='A replacement template cache',
     author='Calvin Spealman',
     author_email='ironfroggy@gmail.com',
