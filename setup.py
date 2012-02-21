1988bf9489e19c83e9839729e94fa283754f8289
Calvin Spealman <calvin@caktusgroup.com>
2012-02-21 13:30:06 -0500

    Marking a 0.6-beta-1 version, and setting up requirements for 2.6 fallback of OrderedDict

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+from sys import version_info
 from setuptools import setup, find_packages
 
+install_requires = [
+    'celery >= 2.4.2',
+    'httplib2 >= 0.6.0',
+]
+if version_info < (2, 7):
+    install_requires.append('ordereddict')
+
 setup(name='bettercache',
-    version='0.5.3',
+    version='0.6-beta-1',
     description = "A suite of better cache tools for Django.",
     license = "MIT",
     author='Calvin Spealman',
     author_email='ironfroggy@gmail.com',
     url='http://github.com/ironfroggy/django-better-cache',
     packages = find_packages(),
-    install_requires = [
-        'celery >= 2.4.2',
-        'httplib2 >= 0.6.0',
-    ],
+    install_requires = install_requires,
 )
