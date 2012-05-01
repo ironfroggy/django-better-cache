a961170994844035ca18cef86ec7c2b412583eb5
Alfredo Deza <alfredodeza@gmail.com>
2012-05-01 10:35:50 -0400

    include a settings file for tests only

---
+++ bettercache/tests/settings.py
@@ -0,0 +1,27 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DEBUG = True
+ROOT_URLCONF = 'proxly.urls'
+INSTALLED_APPS = ( 'bettercache', )
+
+CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        },
+}
+
+DATABASES = {}
+ADMINS = ( )
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = False
+USE_L10N = False
+TEMPLATE_LOADERS = (
+            ('django.template.loaders.cached.Loader', (
+                'django.template.loaders.filesystem.Loader',
+            )),
+)
+TEMPLATE_DIRS = ( os.path.join(PROJECT_ROOT, 'templates'),)
+MIDDLEWARE_CLASSES = ( )
