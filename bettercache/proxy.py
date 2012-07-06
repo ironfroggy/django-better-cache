304e124acff9c7cf712645f558181426d3e9e67b
Alfredo Deza <alfredodeza@gmail.com>
2012-07-06 10:19:02 -0400

    add logging to the proxy module

--- bettercache/proxy.py
+++ bettercache/proxy.py
@@ -7,6 +7,7 @@ cache.
 """
 
 from httplib2 import Http
+import logging
 
 from django.http import HttpResponse
 from django.conf import settings
@@ -26,6 +27,7 @@ except ImportError:
         return header_name.lower() in _hop_headers
 
 HOST = getattr(settings, 'BETTERCACHE_ORIGIN_HOST', 'localhost')
+logger = logging.getLogger(__name__)
 
 if getattr(settings, 'BETTERCACHE_ORIGIN_PORT', None):
     HOST += ":" + str(settings.BETTERCACHE_ORIGIN_PORT)
@@ -54,7 +56,7 @@ def proxy(request):
     for name, val in info.items():
         if not is_hop_by_hop(name):
             response[name] = val
-
+    logger.debug("proxyied request to: %s" % uri)
     return response
 
 
